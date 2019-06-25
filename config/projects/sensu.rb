e3e0ea6adf5fa8a9912b1fde9ee167feb6ead2af
Jared Ledvina <jared@techsmix.net>
2019-06-25 19:17:35 -0400

    Upgrade embeded Ruby to 2.4.6
    Signed-off-by: Jared Ledvina <jared@techsmix.net>

--- config/projects/sensu.rb
+++ config/projects/sensu.rb
@@ -40,7 +40,7 @@ build_version version
 build_iteration ENV["BUILD_NUMBER"]
 
 override "sensu-gem", version: version
-override "ruby", version: "2.4.4"
+override "ruby", version: "2.4.6"
 override "rubygems", version: "2.6.10"
 
 package :deb do
